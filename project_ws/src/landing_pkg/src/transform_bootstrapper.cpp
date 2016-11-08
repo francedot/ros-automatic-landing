a072af309ce46339643bc4629b60bceabbbc7348
francibon93 <francibon93@gmail.com>
2016-11-08 16:49:03 +0100

    Added Tansformation nodes from pose to x, y,z

---
+++ project_ws/src/landing_pkg/src/transform_bootstrapper.cpp
@@ -0,0 +1,26 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+using namespace std;
+using namespace std_msgs;
+
+// a mock callback function
+void positionReceived(const std_msgs::StringConstPtr &msg) {
+	ROS_INFO("Received!");	
+}
+
+int main(int argc, char ** argv) {
+	ros::init(argc, argv, "transform_bootstrapper");
+	ros::NodeHandle nodeHandle;
+	
+	ROS_INFO("transform_bootstrapper initialized!");
+
+	// create a subscriber object
+	// ros::Subscriber sub = nodeHandle.subscribe("camera1/image_raw", 1000, &positionReceived);
+	ros::Subscriber sub_x = nodeHandle.subscribe("/x_pose", 1000, &positionReceived);
+	ros::Subscriber sub_y = nodeHandle.subscribe("/y_pose", 1000, &positionReceived);
+	ros::Subscriber sub_z = nodeHandle.subscribe("/z_pose", 1000, &positionReceived);
+
+	// let ROS take over
+	ros::spin();
+}
