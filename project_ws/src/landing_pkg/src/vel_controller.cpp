bc61dabbf623cb3e59b49499e68ef1a266fc8572
francibon93 <francibon93@gmail.com>
2016-11-19 19:46:19 +0100

    Implemented translation for k_effort

--- project_ws/src/landing_pkg/src/vel_controller.cpp
+++ project_ws/src/landing_pkg/src/vel_controller.cpp
@@ -4,20 +4,40 @@
 #include <message_filters/sync_policies/exact_time.h>
 #include <pid/StampedFloat64.h>
 #include <geometry_msgs/Twist.h>
+#include <tf/transform_listener.h>
 
 using namespace geometry_msgs;
 using namespace pid;
 using namespace message_filters;
 
 ros::Publisher chatter_pub;
+boost::shared_ptr<tf::TransformListener> listener;
+tf::StampedTransform transform;
+bool transform_setup = false;
 
 void effortReceived(const StampedFloat64ConstPtr& xEffort, const StampedFloat64ConstPtr& yEffort) {
-Twist tw;
-tw.linear.x = xEffort->c;
-tw.linear.y = yEffort->c;
-ROS_INFO("This is x: %f", tw.linear.x);
-ROS_INFO("This is y: %f", tw.linear.y);
-//chatter_pub.publish(tw);
+
+  if(!transform_setup) {
+    try{
+      listener->lookupTransform("ardrone_base_link","ardrone_base_bottomcam",ros::Time(0),transform);
+      transform_setup = true;
+    }
+    catch(tf::TransformException &ex) {
+      ROS_ERROR("%s",ex.what());
+      return;
+    }
+  }
+  
+  tf::Vector3 linear(xEffort->c, yEffort->c, 0);
+  linear = (transform * linear) - (transform * tf::Vector3(0,0,0));
+  
+  Twist tw;
+  tw.linear.x = linear.x();
+  tw.linear.y = linear.y();
+
+  ROS_INFO("This is x: %f", tw.linear.x);
+  ROS_INFO("This is y: %f", tw.linear.y);
+  chatter_pub.publish(tw);
 }
 
 int main(int argc, char** argv)
@@ -26,6 +46,7 @@ int main(int argc, char** argv)
 
   ros::NodeHandle nh;
   
+  listener.reset(new tf::TransformListener);
   chatter_pub = nh.advertise<Twist>("cmd_vel", 1);
   
   message_filters::Subscriber<StampedFloat64> x_effort_sub(nh, "x_effort", 1);
