0cbb04be2e46e777a5a33b50412e296528d0d4ce
Molonlabe16 <antoniocaiazza@gmail.com>
2016-12-15 00:19:23 +0100

    Fix update_average error

--- project_ws/src/landing_pkg/src/error_monitor.cpp
+++ project_ws/src/landing_pkg/src/error_monitor.cpp
@@ -12,19 +12,17 @@ double ey = 0.0;
 double dx = 0.0;
 double dy = 0.0;
 
-// Average of the above variables in the last n
-// seconds where n is indicated by average_seconds
+// Average of the above variables taken for the last size_of_average samples
 double avg_ex = 0.0;
 double avg_ey = 0.0;
 double avg_dx = 0.0;
 double avg_dy = 0.0;
-//long avg_index = 0.0;
 
-const int size_of_average = 100;
-double avg_ex_samples[size_of_average];
-double avg_ey_samples[size_of_average];
-double avg_dx_samples[size_of_average];
-double avg_dy_samples[size_of_average];
+const int number_of_samples = 100;
+double avg_ex_samples[number_of_samples];
+double avg_ey_samples[number_of_samples];
+double avg_dx_samples[number_of_samples];
+double avg_dy_samples[number_of_samples];
 
 long iteration = 0;
 
@@ -34,25 +32,17 @@ double last_ey = 0.0;
 ros::Time prev_time;
 ros::Duration delta_t;
 
-//average_seconds set to 7 seconds
-ros::Duration average_seconds = ros::Duration(7,0);
-ros::Time start_of_average;
-
 ros::Publisher error_pub;
 
 void on_error_update(const geometry_msgs::PoseStampedConstPtr &stamped_msg);
-//void update_averages(double new_val, double *avg, long *index);
 double update_average(double new_val, double *average_array, const int size);
 
 
 int main(int argc, char **argv) {
-
     ros::init(argc, argv, "error_monitor");
     ros::NodeHandle nh;
-
     ros::Subscriber pose_sub = nh.subscribe("/ar_single_board/pose", 1, on_error_update);
     error_pub = nh.advertise<landing_pkg::ErrorStamped>("/pose_error", 1);
-
     ros::spin();
     return 0;
 }
@@ -68,12 +58,9 @@ void on_error_update(const geometry_msgs::PoseStampedConstPtr &stamped_msg) {
     }else{
         ex = fabs(stamped_msg->pose.position.x);
         ey = fabs(stamped_msg->pose.position.y);
-        //avg_index = 0;
-        //start_of_average = ros::Time::now();
         prev_time = ros::Time::now();
         return;
     }
-
     // Assuming setpoint = (0,0)
     last_ex = ex;
     last_ey = ey;
@@ -83,30 +70,11 @@ void on_error_update(const geometry_msgs::PoseStampedConstPtr &stamped_msg) {
     dx = fabs(ex - last_ex) / delta_t.toSec();
     dy = fabs(ey - last_ey) / delta_t.toSec();
 
-    avg_ex = update_average(ex,avg_ex_samples, size_of_average);
-    avg_ey = update_average(ey,avg_ey_samples, size_of_average);
-    avg_dx = update_average(dx,avg_dx_samples, size_of_average);
-    avg_dy = update_average(dy,avg_dy_samples, size_of_average);
+    avg_ex = update_average(ex,avg_ex_samples, number_of_samples);
+    avg_ey = update_average(ey,avg_ey_samples, number_of_samples);
+    avg_dx = update_average(dx,avg_dx_samples, number_of_samples);
+    avg_dy = update_average(dy,avg_dy_samples, number_of_samples);
 
-    /*
-
-    if(ros::Time::now() - start_of_average >= average_seconds) {
-        avg_ex = 0;
-        avg_ey = 0;
-        avg_dx = 0;
-        avg_dy = 0;
-        avg_index = 0;
-        start_of_average = ros::Time::now();
-    }else{
-        update_averages(ex, &avg_ex, &avg_index);
-        avg_index--;
-        update_averages(ey, &avg_ey, &avg_index);
-        avg_index--;
-        update_averages(dx, &avg_dx, &avg_index);
-        avg_index--;
-        update_averages(dy, &avg_dy, &avg_index);
-    }
-*/
     landing_pkg::ErrorStamped output_error;
     output_error.header = stamped_msg->header;
     output_error.ex = ex;
@@ -126,19 +94,14 @@ void on_error_update(const geometry_msgs::PoseStampedConstPtr &stamped_msg) {
              output_error.avg_dx, output_error.avg_dy);
 }
 
-/*
-void update_averages(double new_val, double *avg, long *index) {
-    (*index) += 1;
-    double diff = new_val - (*avg);
-    (*avg) = (*avg) + (diff / (*index));
-}*/
-
 double update_average(double new_val, double *average_array, const int size) {
     int index = iteration % size;
     average_array[index] = new_val;
     double avg = 0;
     for(int i = 0; i < size; i++) {
-        avg = average_array[i];
+        avg += average_array[i];
     }
-    return avg/size;
+    if(iteration >= size)
+        return avg/size;
+    return (avg/(double)(iteration+1));
 }
