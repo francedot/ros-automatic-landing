abd5db51b3f4f737128c6359195f457c8ee27464
Molonlabe16 <antoniocaiazza@gmail.com>
2016-12-16 12:41:12 +0100

    Add switch board to lander

--- project_ws/src/landing_pkg/src/lander.cpp
+++ project_ws/src/landing_pkg/src/lander.cpp
@@ -20,10 +20,11 @@ ErrorStamped cur_pose_error;
 double cur_quota;
 double zeta_land_vel = -0.5;
 
-ros::Publisher z_effort_pub;
-/* Aggiunto Fra*/
+
+// Publisher nodes for landing and switching boards
 ros::Publisher switch_board_pub;
 ros::Publisher landing_pub;
+ros::Publisher z_effort_pub;
 
 bool enable_landing = false;
 
@@ -38,36 +39,32 @@ double center_margin = 0.01; // Accepted distance from (0,0) in m
 double speed_margin = 0.02; // Accepted position variation in m/s
 double avg_center_margin = 0.02;
 double avg_speed_margin = 0.02;
-double landing_quote = 300.0; // Quote for landing signal
+double landing_quote = 200.0; // Quote for landing signal
 double recovering_quote = 650.0;
 double recovering_speed = 0.2;
+double switching_board_quote = 300.0;
 
-void cur_quota_received(const NavdataConstPtr &navdata) {
-    cur_quota = navdata->altd;
-//    ROS_INFO("lander: Received quota %lf", cur_quota);
-}
+void cur_quota_received(const NavdataConstPtr &navdata);
 
-void pose_error_received(const ErrorStampedConstPtr &error) {
-    cur_pose_error = *error;
-    /*ROS_INFO(
-            "lander: Received pose_error (ex=%lf, ey=%lf, dx=%lf, dy=%lf, avg_ex=%lf, avg_ey=%lf, avg_dx=%lf, avg_dy=%lf)",
-            cur_pose_error.ex, cur_pose_error.ey, cur_pose_error.dx, cur_pose_error.dy, cur_pose_error.avg_ex,
-            cur_pose_error.avg_ey,
-            cur_pose_error.avg_dx, cur_pose_error.avg_dy);*/
-}
+void pose_error_received(const ErrorStampedConstPtr &error);
 
-void landing_boolean_received(const std_msgs::EmptyConstPtr &e) {
-    enable_landing = !enable_landing;
-}
+void landing_boolean_received(const std_msgs::EmptyConstPtr &e);
 
-bool check_landing_conditions() {
-    double k = 1;
-    if(descending == drone_status)
-        k = 2.0;
-    return ((cur_pose_error.ex < (k * center_margin)) && (cur_pose_error.ey < (k * center_margin)) &&
-            (cur_pose_error.dx < (k * speed_margin)) && (cur_pose_error.dy < (k * speed_margin)) &&
-            (cur_pose_error.avg_ex < (k * avg_center_margin)) && (cur_pose_error.avg_ey < (k * avg_center_margin)) &&
-            (cur_pose_error.avg_dx < (k * avg_speed_margin)) && (cur_pose_error.avg_dy < (k * avg_speed_margin)));
+bool check_landing_conditions();
+
+void switch_board() {
+    std_msgs::String board;
+    switch (drone_status) {
+        case descending: {
+            board.data = "inner";
+            } break;
+        case rising: {
+            board.data = "outer";
+            } break;
+        ROS_ERROR("Switch board called in state: %d.\n", drone_status);
+    }
+
+    switch_board_pub.publish(board);
 }
 
 int main(int argc, char **argv) {
@@ -80,14 +77,11 @@ int main(int argc, char **argv) {
     pnh.param("avg_center_margin",avg_center_margin,0.02);
     pnh.param("avg_speed_margin",avg_speed_margin,0.02);
     pnh.param("landing_quote",landing_quote,300.0);
-    landing_pub = nh.advertise<Empty>("/ardrone/land",1);
-    z_effort_pub = nh.advertise<Float64>("/z_effort",1);
 
-    /* Aggiunto Fra*/
+    z_effort_pub = nh.advertise<Float64>("/z_effort",1);
+    landing_pub = nh.advertise<Empty>("/ardrone/land",1);
     switch_board_pub = nh.advertise<String>("/switch_board",1);
 
-    // create a subscriber object
-   // ros::Subscriber sub_odometry = nh.subscribe("/ardrone/odometry",1,&odometry_received);
     ros::Subscriber sub_reach_quota = nh.subscribe("/ardrone/navdata", 1, &cur_quota_received);
     ros::Subscriber sub_pose_error = nh.subscribe("/pose_error", 1, &pose_error_received);
     ros::Subscriber sub_landing_enable = nh.subscribe("/enable_land", 1, &landing_boolean_received);
@@ -115,6 +109,8 @@ int main(int argc, char **argv) {
                     break;
                 case descending: {
                     ROS_INFO("Status: Descending. Alt: %lf.",cur_quota);
+                    if(cur_quota <= switching_board_quote)
+                        switch_board();
                     if (cur_quota <= landing_quote) {
                         drone_status = landing;
                         break;
@@ -137,6 +133,8 @@ int main(int argc, char **argv) {
                     break;
                 case rising: {
                     ROS_INFO("Status: Rising. Alt: %lf.",cur_quota);
+                    if(cur_quota > switching_board_quote)
+                        switch_board();
                     if(cur_quota >= recovering_quote) {
                         z_effort.data = 0;
                         drone_status = stabilizing;
@@ -155,3 +153,31 @@ int main(int argc, char **argv) {
     return 0;
 }
 
+void cur_quota_received(const NavdataConstPtr &navdata) {
+    cur_quota = navdata->altd;
+}
+
+void pose_error_received(const ErrorStampedConstPtr &error) {
+    cur_pose_error = *error;
+    /*ROS_INFO(
+            "lander: Received pose_error (ex=%lf, ey=%lf, dx=%lf, dy=%lf, avg_ex=%lf, avg_ey=%lf, avg_dx=%lf, avg_dy=%lf)",
+            cur_pose_error.ex, cur_pose_error.ey, cur_pose_error.dx, cur_pose_error.dy, cur_pose_error.avg_ex,
+            cur_pose_error.avg_ey,
+            cur_pose_error.avg_dx, cur_pose_error.avg_dy);*/
+}
+
+void landing_boolean_received(const std_msgs::EmptyConstPtr &e) {
+    enable_landing = !enable_landing;
+}
+
+
+
+bool check_landing_conditions() {
+    double k = 1;
+    if(descending == drone_status)
+        k = 2.0;
+    return ((cur_pose_error.ex < (k * center_margin)) && (cur_pose_error.ey < (k * center_margin)) &&
+            (cur_pose_error.dx < (k * speed_margin)) && (cur_pose_error.dy < (k * speed_margin)) &&
+            (cur_pose_error.avg_ex < (k * avg_center_margin)) && (cur_pose_error.avg_ey < (k * avg_center_margin)) &&
+            (cur_pose_error.avg_dx < (k * avg_speed_margin)) && (cur_pose_error.avg_dy < (k * avg_speed_margin)));
+}
